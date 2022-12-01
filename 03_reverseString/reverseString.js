1cf07eab03d77d710721f65ef8603357b948f204
Sondre Instanes <Fyrren@gmail.com>
2022-12-01 14:37:08 +0100

    Third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let split = words.split("");
+    let reverse = split.reverse();
+    let result = reverse.join("");
+    return result;
 };
 
 // Do not edit below this line
