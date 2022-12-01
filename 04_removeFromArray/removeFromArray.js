1c32ccc2f644f8a911bd1cf9ed2aea2ff4f12a39
Sondre Instanes <Fyrren@gmail.com>
2022-12-01 15:13:49 +0100

    stupid fucking 4th exericse

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+//fuck this pice of shit exercise, what the fuck is a rest operator
+const removeFromArray = function(...arrayName) {
+    const array = arrayName[0];
+    const newArray = [];
 
+    array.forEach((item) => {
+        if (!arrayName.includes(item)) {
+            newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
