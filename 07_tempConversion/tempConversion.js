d638207bd904cf1d745e880115525aefacb2a213
Sondre Instanes <Fyrren@gmail.com>
2022-12-01 19:49:14 +0100

    Seventh Exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const temp = (fahrenheit - 32) * 5 / 9;
+  const result = Math.round(temp * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const temp = celsius * 9 / 5 + 32;
+  const result = Math.round(temp * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
