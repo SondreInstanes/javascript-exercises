c1f6b0ec94f9c80d7c2db82538d18361ae0c55cc
Sondre Instanes <Fyrren@gmail.com>
2022-12-01 18:43:47 +0100

    Fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let result = 0;
+    if (first > last) {
+        const temp = first;
+        first = last;
+        last = temp;
+    }
 
+    if (first < 0 || last < 0) return 'ERROR';
+    if (!Number.isInteger(first) || !Number.isInteger(last)) return 'ERROR';
+
+    for (let i = first; i < last + 1; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
