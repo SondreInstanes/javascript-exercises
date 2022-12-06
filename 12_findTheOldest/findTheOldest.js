b271b53121c99f2a747d49b9fa017ab8de40af73
Sondre Instanes <Fyrren@gmail.com>
2022-12-06 16:34:23 +0100

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(args) {
+    return args.reduce((result, person) => {
+        const oldest = age(result.yearOfBirth, result.yearOfDeath);
+        const current = age(person.yearOfBirth, person.yearOfDeath);
+        return oldest < current ? person : result;
+    })
 };
+
+function age (birthYear, deathYear) {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear();
+    }
+    return deathYear - birthYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
