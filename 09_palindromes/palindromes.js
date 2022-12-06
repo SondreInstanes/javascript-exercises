df0b7f1f5bbe23c366c3599dafd7f1ffb4904c1c
Sondre Instanes <Fyrren@gmail.com>
2022-12-06 15:54:05 +0100

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // /W removes all non-alphanumerical characters, /g is global search
+    let remove = /[\W_]/g;
+    // Replace here removes spaces and dots, replacing them with an empty string
+    let lowerStr = str.toLowerCase().replace(remove, "");
+    // This splits the string into individual letters, 
+    // reverses it and concatonates it
+    let reverseStr = lowerStr.split('').reverse().join('');
+
+    // Returns wether strings match or not
+    return reverseStr === lowerStr;
 
 };
 
