daae6065442304b20a3e2b3e14cc3a10b46c822a
Sondre Instanes <Fyrren@gmail.com>
2022-12-06 16:10:13 +0100

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(args) {
+    if (args < 0) return "OOPS";
+    if (args == 0) return 0;
+    let num = 0;
+    let result = 1;
 
+    for (let i = 1; i < args; i++) {
+        const temp = result;
+        result = num + result;
+        num = temp;
+    }
+    return result;
 };
 
 // Do not edit below this line
